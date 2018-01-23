b331da8bc6dde62af598bfb36826723ad1b53d6d
ruskeezy <christian.ax.miller@gmail.com>
2018-01-22 22:03:51 -0800

    added demo code

--- server.js
+++ server.js
@@ -15,9 +15,34 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/api/v1/books', (req, res) => {
-  res.send('<p>Hi</p>');
-})
+app.get('/db/person', function(request, response) {
+  client.query('SELECT * FROM persons;')
+  .then(function(data) {
+    response.send(data);
+  })
+  .catch(function(err) {
+    console.error(err);
+  });
+});
+
+app.post('/db/person', function(request, response) {
+  client.query(`
+    INSERT INTO persons(name, age, ninja)
+    VALUES($1, $2, $3);
+    `,
+    [
+      request.body.name,
+      request.body.age,
+      request.body.ninja
+    ]
+  )
+  .then(function(data) {
+    response.redirect('/');
+  })
+  .catch(function(err) {
+    console.error(err);
+  });
+});
 
 app.listen(PORT, () => {
   console.log('Listening on PORT: ', PORT);

b238e78d135423cefb5c995d7793409e7f01515f
ruskeezy <christian.ax.miller@gmail.com>
2018-01-25 09:36:33 -0800

    fixed constring, removed seed function

--- server.js
+++ server.js
@@ -1,14 +1,13 @@
 'use strict';
 
 const cors = require('cors');
-const fs = require('fs');
 const express = require('express');
 const pg = require('pg');
 const bodyParser = require('body-parser');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-const connectionString = process.env.DATABASE_URL;
+const connectionString = 'postgres://jwgwshirxcmdod:e69125df1cf81cc7e55e9ec8782829e58c32114b3e01ba99645b01029c53c79c@ec2-54-163-237-249.compute-1.amazonaws.com:5432/d9ur96v0cd5qio';
 const client = new pg.Client(connectionString);
 client.connect();
 
@@ -16,15 +15,15 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('DATABASE_URL', function(request, response) {
-  client.query('SELECT * FROM DATABASE_URL;')
-    .then(function(data) {
-      response.send(data);
-    })
-    .catch(function(err) {
-      console.error(err);
-    });
-});
+// app.get('DATABASE_URL', function(request, response) {
+//   client.query('SELECT * FROM DATABASE_URL;')
+//     .then(function(data) {
+//       response.send(data);
+//     })
+//     .catch(function(err) {
+//       console.error(err);
+//     });
+// });
 
 app.post('/books', (req, res) => {
   client.query(`INSERT INTO books (author, title, img_url, isbn, description) VALUES ($1, $2, $3, $4, $5);`, 
@@ -43,25 +42,6 @@ app.post('/books', (req, res) => {
     })
 })
 
-function seedBooks() {
-  fs.readFile('./book-list-client/data/book.json', function(err, fd) {
-    JSON.parse(fd.toString()).forEach(function(ele) {
-      client.query(
-        'INSERT INTO books(title, author, isbn, image_url, description) VALUES($1, $2, $3, $4, $5) ON CONFLICT DO NOTHING',
-        [ele.title, ele.author, ele.isbn, ele.image_url, ele.description]
-      )
-    })
-  })
-}
-
-
-app.get('/', (req, res) => {
-  res.send('Test');
-})
-
-
 app.listen(PORT, () => {
   console.log('Listening on PORT: ', PORT);
 })
-
-seedBooks();

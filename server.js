a18bff8411f4e57ef7ddf97fa77c753dad1f5642
ruskeezy <christian.ax.miller@gmail.com>
2018-01-25 10:53:49 -0800

    fixed constring

--- server.js
+++ server.js
@@ -1,19 +1,36 @@
 'use strict';
 
+// Require dependancies
 const cors = require('cors');
 const express = require('express');
 const pg = require('pg');
 const bodyParser = require('body-parser');
+
+// Initialize express, constring
 const app = express();
 const PORT = process.env.PORT || 3000;
-
-const connectionString = 'postgres://jwgwshirxcmdod:e69125df1cf81cc7e55e9ec8782829e58c32114b3e01ba99645b01029c53c79c@ec2-54-163-237-249.compute-1.amazonaws.com:5432/d9ur96v0cd5qio';
+process.env.DATABASE_URL = 'postgres://postgres:1234@localhost:5432/books_app';
+const connectionString = process.env.DATABASE_URL;
+console.log('connectionString: ',connectionString);
 const client = new pg.Client(connectionString);
+
+// connect to database
 client.connect();
 
 app.use(cors());
+app.use(function(req, res, next) {
+  res.header("Acess-Control-Allow_Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/api/v1/books', (req, res) => {
+  client.query('SELECT book_id, author, title, img_url FROM books;')
+    .then(result => res.send(result.rows));
+})
 
 // app.get('DATABASE_URL', function(request, response) {
 //   client.query('SELECT * FROM DATABASE_URL;')

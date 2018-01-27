2a8e664e27bb94d4b4acd3d3ca3bed0ca54d5c66
Jay Adams <j.adams011@hotmail.com>
2018-01-26 17:10:42 -0800

    work on filling out server.js file

--- server.js
+++ server.js
@@ -33,14 +33,29 @@ app.get('/api/v1/books', (req, res) => {
     .then(result => res.send(result.rows));
 })
 
-app.get('/api/v1/books/:id', (req, res) => {
-  client.query(
-    'SELECT * FROM books WHERE book_id=$1;',
-    [req.params.id])
-    .then(result => res.send(result.rows))
-})
+app.get('/v1/books', function(req, res) {
+  // console.log('app.get /v1/books');
+  client.query('SELECT * FROM books;')
+    .then(function(data) {
+      res.send(data.rows);
+    })
+    .catch(function(err) {
+      console.error(err);
+    });
+});
 
-app.post('/books', (req, res) => {
+app.get('/v1/books/:book_id', function (req,res) {
+  // console.log(req);
+  client.query(`SELECT * FROM books WHERE book_id = ${req.params.book_id};`)
+    .then(function(data){
+      res.send(data.rows);
+    })
+    .catch(function(err) { 
+      console.log(err);
+    });
+});
+
+app.post('/v1/books', (req, res) => {
   client.query(`INSERT INTO books (author, title, img_url, isbn, description) VALUES ($1, $2, $3, $4, $5);`, 
     [
       req.body.author,
@@ -57,6 +72,55 @@ app.post('/books', (req, res) => {
     })
 })
 
+// DELETE
+app.delete('/v1/books/:book_id', function(req, res) {
+  console.log(req.params.book_id);
+  client.query(`DELETE FROM books WHERE book_id=$1`,
+    [req.params.book_id]
+  )
+    .then(() => res.send('Delete complete'))
+    .catch(console.error);
+});
+
+// UPDATE/PUT
+app.put('/v1/books/:book_id/edit', function(req, res) {
+  console.log(req.body);
+  client.query(`UPDATE * FROM books WHERE book_id = ${req.params.book_id};`)
+    .then(() => {
+      client.query(`
+      UPDATE books
+      SET title=$1, author=$2, isbn=$3, image_url=$4, description=$5
+      WHERE book_id = $6
+      `,
+        [
+          req.body.title,
+          req.body.author,
+          req.body.isbn,
+          req.body.image_url,
+          req.body.description,
+          req.params.book_id
+        ]
+      )
+    })
+    .then(() => res.send('Update complete'))
+    .catch(console.error);
+});
+
+createTable();
+
 app.listen(PORT, () => {
-  console.log('Listening on PORT: ', PORT);
-})
+  console.log('SERVER started on port:', PORT);
+});
+
+function createTable() {
+  client.query(`
+    CREATE TABLE IF NOT EXISTS books(
+      book_id SERIAL PRIMARY KEY,
+      title VARCHAR(255),
+      author VARCHAR(255),
+      isbn VARCHAR(255),
+      image_url VARCHAR(255),
+      description TEXT NOT NULL
+    );`
+  )
+}
